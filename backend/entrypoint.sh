d2d6904882f84f278a12c5baabeb1736b9e40d35
Александр Солдатов <p90_master@mail.ru>
2025-01-17 20:34:54 +0300

    BACKEND: backend-ng becomes main & logging conf for fastapi

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,5 +2,5 @@
 
 # TODO: production-ready configuration & get params from .env
 # TODO: MOVE uvicorn to main.py
-uvicorn app.main:app --host 0.0.0.0 --port 8000 \
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --log-config log_conf.yaml \
   --timeout-keep-alive 30 --workers 1 --limit-concurrency 10
