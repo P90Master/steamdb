5cf874f98e0181762a6e972dd56d9c96f38c900d
Александр Солдатов <p90_master@mail.ru>
2025-01-26 14:44:52 +0300

    PROJECT: docker env hotifix

--- orchestrator/entrypoint.sh
+++ orchestrator/entrypoint.sh
@@ -5,10 +5,10 @@ alembic -c orchestrator/db/alembic.ini upgrade head
 python -m orchestrator.messenger &
 celery -A orchestrator.celery.beat_app beat --loglevel=error &
 celery -A orchestrator.celery.worker_app worker --loglevel=error &
-uvicorn app.main:app \
+uvicorn orchestrator.api.main:app \
   --host 0.0.0.0 \
   --port ${PORT:-8888} \
-  --log-config log_conf.yaml \
+  --log-config api_log_conf.yaml \
   --timeout-keep-alive ${TIMEOUT_KEEP_ALIVE:-60} \
   --workers ${WORKERS:-4} \
   --limit-concurrency ${LIMIT_CONCURRENCY:-100} \
