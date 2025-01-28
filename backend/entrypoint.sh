ff499d488ab8764b51b86804412ee2142aeebcc2
Александр Солдатов <p90_master@mail.ru>
2025-01-28 23:33:09 +0300

    BACKEND: replication & nginx load balancing

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,7 +6,7 @@ uvicorn app.main:app \
   --log-config log_conf.yaml \
   --timeout-keep-alive ${TIMEOUT_KEEP_ALIVE:-60} \
   --workers ${WORKERS:-4} \
-  --limit-concurrency ${LIMIT_CONCURRENCY:-100} \
+  --limit-concurrency ${LIMIT_CONCURRENCY:-4096} \
   --backlog ${BACKLOG_SIZE:-2048} \
   --proxy-headers \
   --forwarded-allow-ips=${FORWARDED_ALLOW_IPS:-'*'}
--- orchestrator/entrypoint.sh
+++ orchestrator/entrypoint.sh
@@ -11,7 +11,7 @@ uvicorn orchestrator.api.main:app \
   --log-config api_log_conf.yaml \
   --timeout-keep-alive ${TIMEOUT_KEEP_ALIVE:-60} \
   --workers ${WORKERS:-4} \
-  --limit-concurrency ${LIMIT_CONCURRENCY:-100} \
+  --limit-concurrency ${LIMIT_CONCURRENCY:-4096} \
   --backlog ${BACKLOG_SIZE:-2048} \
   --proxy-headers \
   --forwarded-allow-ips=${FORWARDED_ALLOW_IPS:-'*'}
